b614e037ace80eb330d827f32732bb499a232d91
Rabiulrn <rnrabiul@gmail.com>
2023-12-01 11:31:58 +0600

    login registration done

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthManager;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[UserController::class,'index'])->name('home');
+// Route::get('/register',[UserController::class,'register'])->name('register');
+Route::get('/register',[AuthManager::class,'register'])->name('register');
+Route::post('/registerPost',[AuthManager::class,'registerPost'])->name('register.post');
+Route::get('/login',[AuthManager::class,'login'])->name('login');
+Route::post('/loginPost',[AuthManager::class,'loginPost'])->name('login.post');
